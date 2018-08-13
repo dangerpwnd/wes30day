618b6f28f3ef0be57afed10a28fefc14d0f42f7e
Eric Dangerstone <reric2012@yahoo.com>
2018-08-12 20:33:54 -0500

    Added skip and progress bar functionality

--- Day 11 - Custom Video Player/vid.js
+++ Day 11 - Custom Video Player/vid.js
@@ -1,12 +1,12 @@
 /* Build out elements */
 
-const player = document.querySelector(".player");
-const video = player.querySelector(".viewer");
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
-const progress_filled = player.querySelector('progress_filled');
+const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelector('[data-skip]');
-const ranges = player.querySelector('.player_slider');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
 
 /* Create functions */
 
@@ -26,8 +26,43 @@ function updateButton(){
   toggle.textContent = icon;
 }
 
+function skip(){
+  //console.log('Skipping!')
+  console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name]=[this.value];
+  console.log(this.value);
+  console.log(this.name);
+}
+
+function handleProgress(){
+  const percent= (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e){
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+  console.log(e);
+}
+
 /*Hook up the event listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+
+//ADD FULLSCREEN BUTTON**
